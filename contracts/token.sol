1de0cdffdbcb91e1232cb3f1b852111f809ef701
husna-fathima-713 <husnafathima_icb2023@ksit.edu.in>
2026-06-19 21:46:05 +0530

    Add professional audit dashboard styling

--- contracts/token.sol
+++ contracts/token.sol
@@ -2,9 +2,7 @@ pragma solidity ^0.8.0;
 
 contract Token {
 
-    mapping(address => uint256) balances;
+    function mint(address user, uint amount) public {
 
-    function mint(address user, uint256 amount) public {
-        balances[user] += amount;
     }
 }
