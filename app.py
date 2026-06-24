7b72dd6b27b244dd0e7266e7e2552fd97303baf9
husna-fathima-713 <husnafathima_icb2023@ksit.edu.in>
2026-06-24 23:18:34 +0530

    Update App.py

--- app.py
+++ app.py
@@ -1,20 +1,43 @@
-from flask import Flask, render_template, request
+from flask import (
+    Flask,
+    render_template,
+    request
+)
+
 from src.analyzer import analyze_contract
 from src.report_generator import generate_report
+
+from src.database import (
+    initialize_database,
+    save_audit,
+    get_all_audits
+)
+
+from datetime import datetime
+
 import os
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = "contracts"
 
+initialize_database()
+
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+
+    return render_template(
+        "index.html"
+    )
+
 
 @app.route("/upload", methods=["POST"])
 def upload():
 
-    file = request.files.get("contract")
+    file = request.files.get(
+        "contract"
+    )
 
     if not file:
         return "No file selected"
@@ -26,14 +49,37 @@ def upload():
 
     file.save(filepath)
 
-    result = analyze_contract(filepath)
+    result = analyze_contract(
+        filepath
+    )
 
     generate_report(result)
+
+    save_audit(
+        result["contract_name"],
+        result["risk_level"],
+        result["total_findings"],
+        datetime.now().strftime(
+            "%Y-%m-%d %H:%M:%S"
+        )
+    )
 
     return render_template(
         "report.html",
         result=result
     )
+
+
+@app.route("/history")
+def history():
+
+    audits = get_all_audits()
+
+    return render_template(
+        "history.html",
+        audits=audits
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
