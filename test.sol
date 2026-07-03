195760869ef74876ba5b3a7694d07dd6d0ab79b6
husna-fathima-713 <husnafathima_icb2023@ksit.edu.in>
2026-07-03 20:48:53 +0530

    Enhance parser with function metadata and state variables

--- test.sol
+++ test.sol
@@ -1,10 +1,20 @@
-pragma solidity ^0.7.0;
+pragma solidity ^0.8.0;
 
-contract OverflowTest {
+contract Bank {
 
-    uint256 balance;
+    address owner;
 
-    function addFunds(uint256 amount) public {
-        balance = balance + amount;
+    mapping(address => uint) balances;
+
+    function deposit(uint amount) public payable {
+
+    }
+
+    function withdraw(uint amount) public {
+
+    }
+
+    function emergencyWithdraw() public onlyOwner {
+
     }
 }
