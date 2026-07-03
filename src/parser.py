2f1b3167ed032b3bc7c1e9a3e9f47dff66932910
husna-fathima-713 <husnafathima_icb2023@ksit.edu.in>
2026-07-03 20:46:31 +0530

    Update parser

--- src/parser.py
+++ src/parser.py
@@ -15,13 +15,70 @@ def parse_contract(code):
     )
 
     function_pattern = (
-        r"function\s+(\w+)\s*\("
+        r"function\s+(\w+)\s*\((.*?)\)\s*([^{]*)"
     )
 
-    functions = re.findall(
-        function_pattern,
-        code
-    )
+    functions = []
+
+    for match in re.finditer(function_pattern, code, re.DOTALL):
+
+        name = match.group(1)
+
+        parameters = match.group(2).strip()
+
+        declaration = match.group(3)
+
+        visibility = "default"
+
+        for keyword in [
+            "public",
+            "private",
+            "external",
+            "internal"
+        ]:
+
+            if keyword in declaration:
+                visibility = keyword
+                break
+
+        modifiers = []
+
+        tokens = declaration.split()
+
+        ignore = {
+            "public",
+            "private",
+            "external",
+            "internal",
+            "view",
+            "pure",
+            "payable",
+            "virtual",
+            "override",
+            "returns"
+        }
+
+        for token in tokens:
+
+            token = token.strip()
+
+            if token not in ignore:
+
+                if "(" not in token:
+
+                    modifiers.append(token)
+
+        functions.append({
+
+            "name": name,
+
+            "parameters": parameters,
+
+            "visibility": visibility,
+
+            "modifiers": modifiers
+
+        })
 
     mapping_pattern = (
         r"mapping\s*\([^)]*\)\s*(?:public|private|internal)?\s*(\w+)"
@@ -32,11 +89,23 @@ def parse_contract(code):
         code
     )
 
+    state_variable_pattern = (
+        r"(?:uint|int|bool|address|string|bytes\d*|bytes)\s+(?:public|private|internal)?\s*(\w+)"
+    )
+
+    state_variables = re.findall(
+        state_variable_pattern,
+        code
+    )
+
     return {
 
         "contract_name": contract_name,
 
         "functions": functions,
 
-        "mappings": mappings
+        "mappings": mappings,
+
+        "state_variables": state_variables
+
     }
